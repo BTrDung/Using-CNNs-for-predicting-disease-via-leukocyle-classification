6256158d99c31f2ed70f71c6baa85b04df8d8171
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-06-10 15:30:13 +0700

    Update init_data.py

--- init_data.py
+++ init_data.py
@@ -1,13 +1,5 @@
 import collections
 import numpy as np 
-import tensorflow as tf 
-import matplotlib.pyplot as plt 
-import matplotlib.image as mpimg 
-
-from tensorflow.keras.layers import Convolution2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
  
 from PIL import Image
 
@@ -18,34 +10,35 @@ def getImg(link):
         # img.show()
         return np.array(img)
     return None 
+  
+xTrain = [] 
+yTrain = [] 
+import os 
 
-img     = [ [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\EOSINOPHIL\_0_5239.jpeg')], 
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\EOSINOPHIL\_1_5031.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\EOSINOPHIL\_2_1226.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\EOSINOPHIL\_3_625.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\EOSINOPHIL\_4_8814.jpeg')],
-            
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\LYMPHOCYTE\_0_3975.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\LYMPHOCYTE\_1_4044.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\LYMPHOCYTE\_2_6981.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\LYMPHOCYTE\_3_7545.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\LYMPHOCYTE\_4_2908.jpeg')],
+arr = os.listdir('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\EOSINOPHIL')
+for link in arr: 
+    xTrain.append([getImg('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\EOSINOPHIL\\' + link)])
+    yTrain.append([0])
 
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\MONOCYTE\_0_5020.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\MONOCYTE\_1_4511.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\MONOCYTE\_2_4392.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\MONOCYTE\_3_9457.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\MONOCYTE\_3_9457.jpeg')],
 
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\\NEUTROPHIL\_0_1966.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\\NEUTROPHIL\_1_2118.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\\NEUTROPHIL\_2_1918.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\\NEUTROPHIL\_3_1003.jpeg')],
-            [getImg('E:\SelfStudyTryHard\CS213\dataset2-master\images\TEST_SIMPLE\\NEUTROPHIL\_4_1395.jpeg')]
-        ]
+arr = os.listdir('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\LYMPHOCYTE')
+for link in arr: 
+    xTrain.append([getImg('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\LYMPHOCYTE\\' + link)])
+    yTrain.append([1])
 
-img     = np.array(img)
-img     = img.reshape(20, 240, 320, 3)
-xTrain  = img 
-yTrain = np.array([[0], [0], [0], [0], [0], [1], [1], [1], [1], [1], [2], [2], [2], [2], [2], [3], [3], [3], [3], [3]])
- 
+
+arr = os.listdir('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\MONOCYTE\\')
+for link in arr: 
+    xTrain.append([getImg('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\MONOCYTE\\' + link)])
+    yTrain.append([2])
+
+
+arr = os.listdir('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\NEUTROPHIL')
+for link in arr: 
+    xTrain.append([getImg('E:\\SelfStudyTryHard\\CS213\\dataset2-master\\images\\TRAIN_SIMPLE\\NEUTROPHIL\\' + link)])
+    yTrain.append([3])
+
+
+xTrain = np.array(xTrain)  
+xTrain = xTrain.reshape(len(xTrain), 240, 320, 3)
+yTrain = np.array(yTrain)
