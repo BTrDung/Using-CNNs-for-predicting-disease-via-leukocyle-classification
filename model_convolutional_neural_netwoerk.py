a729cafff764fa0ae92628eb5daecbd9364a6e8e
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-06-10 14:57:09 +0700

    Update model_convolutional_neural_netwoerk.py

--- model_convolutional_neural_netwoerk.py
+++ model_convolutional_neural_netwoerk.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg 
 
+from init_data import xTrain, yTrain
 from tensorflow.keras.layers import Convolution2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
- 
+
 from PIL import Image
 
 # -------------------------------------------------------------------------------------------------
@@ -44,11 +45,11 @@ def get_conv(filter, color, org_img):
     arr     = [[0 for col in range(0, columns - (len(filter) - 1))] for row in range(0,rows - (len(filter) - 1))]
     for row in range(0, rows - (len(filter) - 1)): 
         for column in range(0, columns - (len(filter) - 1)): 
-            # value = 0.0 
-            # for i in range(0, len(filter)): 
-            #     for j in range(0, len(filter)): 
-            #         value = value + (img[0][row + i][column + j][color] * filter[i][j])
-            value = img[0][row][column][color] 
+            value = 0.0 
+            for i in range(0, len(filter)): 
+                for j in range(0, len(filter)): 
+                    value = value + (img[0][row + i][column + j][color] * filter[i][j])
+            # value = img[0][row][column][color] 
             arr[row][column] = value
     return arr
 # -------------------------------------------------------------------------------------------------
@@ -56,9 +57,9 @@ def remake_img(filters, org_img):
     img = np.copy(org_img) 
     print('Getting RED color filter...')
     r   = get_conv(filters[0] ,0 ,org_img)
-    m = np.copy(r) 
-    m = Image.fromarray(m, 'L')
-    m.show()
+    # m = np.copy(r) 
+    # m = Image.fromarray(m, 'L')
+    # m.show()
     print('Getting BLUE color filter...')
     g   = get_conv(filters[1] ,1 ,org_img)
     print('Getting GREEN color filter...')
@@ -75,38 +76,28 @@ def getImg(link):
     return None 
 # -------------------------------------------------------------------------------------------------
 model = tf.keras.models.Sequential([
-    Convolution2D(filters=1, kernel_size=(3, 3), activation="relu", input_shape=(240, 320, 3)), 
+    Convolution2D(filters=32, kernel_size=(3, 3), activation="relu", input_shape=(240, 320, 3)), 
     MaxPooling2D(pool_size=(2, 2)), 
     Convolution2D(filters=64, kernel_size=(3, 3), activation="relu"), 
     MaxPooling2D(pool_size=(2, 2)), 
     Convolution2D(filters=128, kernel_size=(3, 3), activation="relu"), 
     MaxPooling2D(pool_size=(2, 2)), 
+
     Flatten(), 
-    Dense(units=128, activation="relu"),
     Dense(4, activation = "softmax")
 ]) 
 model.summary() 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # ------------------------------------------------------------------------------------------------- 
-# model_ = model.layers[:4]
-# print(model.layers[2].weights)
-
-img     = getImg('E:\SelfStudyTryHard\CS213\_0_187.jpeg')
-rows    = img.shape[0] 
-columns  = img.shape[1] 
-img     = img.reshape(1, rows, columns, 3)
-xTrain = img 
-yTrain = np.array([[1]])
-model.fit(xTrain, yTrain, epochs=50)
- 
-# print(img)
-# print(model.predict(img))
-# print(get_layers(model.layers[0].weights, color=0,rows=3, columns=3, layer=0))
-
+model.fit(xTrain, yTrain, batch_size = 5, epochs=100, verbose=1)
 filter_layers =[get_layers(model.layers[0].weights, color=0,rows=3, columns=3, layer=0), 
                 get_layers(model.layers[0].weights, color=1,rows=3, columns=3, layer=0), 
                 get_layers(model.layers[0].weights, color=2,rows=3, columns=3, layer=0)]
 
-remake_img(filter_layers, img)
+# remake_img(filter_layers, img)
 # model.fit(xTrain, yTrain, batch_size=1, validation_split=0.2, epochs=5)
+# model_ = model.layers[:4]
+# # print(model.layers[2].weights)
+# print(model.predict(img))
+# print(get_layers(model.layers[0].weights, color=0,rows=3, columns=3, layer=0))
 # -----------------------------------------------
