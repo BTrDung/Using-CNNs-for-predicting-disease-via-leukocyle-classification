86f5ab93a7f1ef25e8e1b932f573388a4c9df433
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-06-11 20:23:33 +0700

    Update model_convolutional_neural_netwoerk.py

--- model_convolutional_neural_netwoerk.py
+++ model_convolutional_neural_netwoerk.py
@@ -9,87 +9,55 @@ from tensorflow.keras.layers import Convolution2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.models import load_model
 
 from PIL import Image
 
 # -------------------------------------------------------------------------------------------------
-def get_layers(all_weights, color, rows, columns, layer): 
-    # [0][color][row][column][layer] - Layer should be subtracted by 1. 
-    filter_layer = [] 
-    print(all_weights[0])
-    for row in range(0, rows): 
-        col = [] 
-        for column in range(0, columns): 
-            col.append(all_weights[0][color][row][column][layer].numpy())
-        filter_layer.append(col)
-    return filter_layer
-    
 # -------------------------------------------------------------------------------------------------
-def combine(r, g, b): 
-    rows = len(r) 
-    columns = len(r[0])
-    arr = [] 
-    for row in range(0, rows): 
-        sub = [[r[row][column], g[row][column], b[row][column]] for column in range(0, columns)]
-        arr.append(sub)
-    print('Converting a array to image...')
-    arr = np.array(arr)
-    img = Image.fromarray(arr, 'RGB')
-    img.show()
-    return None
-# -------------------------------------------------------------------------------------------------
-def get_conv(filter, color, org_img): 
-    img     = np.copy(org_img) 
-    rows    = img.shape[1] 
-    columns = img.shape[2] 
-    arr     = [[0 for col in range(0, columns - (len(filter) - 1))] for row in range(0,rows - (len(filter) - 1))]
-    for row in range(0, rows - (len(filter) - 1)): 
-        for column in range(0, columns - (len(filter) - 1)): 
-            value = 0.0 
-            for i in range(0, len(filter)): 
-                for j in range(0, len(filter)): 
-                    value = value + (img[0][row + i][column + j][color] * filter[i][j])
-            # value = img[0][row][column][color] 
-            arr[row][column] = value
-    return arr
-# -------------------------------------------------------------------------------------------------
-def remake_img(filters, org_img): 
-    img = np.copy(org_img) 
-    print('Getting RED color filter...')
-    r   = get_conv(filters[0] ,0 ,org_img)
-    # m = np.copy(r) 
-    # m = Image.fromarray(m, 'L')
-    # m.show()
-    print('Getting BLUE color filter...')
-    g   = get_conv(filters[1] ,1 ,org_img)
-    print('Getting GREEN color filter...')
-    b   = get_conv(filters[2] ,2 ,org_img)
-    rbg = combine(r, g, b) 
-    return None
-# -------------------------------------------------------------------------------------------------
-def getImg(link): 
-    with open(link, 'rb') as f: 
-        img = Image.open(f)
-        img = img.convert('RGB')
-        # img.show()
-        return np.array(img)
-    return None 
+def plotFilters(conv_filter): 
+    fig, axes = plt.subplots(1, 3, figsize = (5, 5)) 
+    axes = axes.flatten() 
+    for img, ax in zip(conv_filter, axes): 
+        ax.imshow(img) 
+        ax.axis('off') 
+    plt.tight_layout()
+    plt.show()
+
+def visualize_all_filter(model): 
+    for layer in model.layers:
+        if 'conv' in layer.name:
+            filters, bias= layer.get_weights()
+            print(layer.name, filters.shape)
+            #normalize filter values between  0 and 1 for visualization
+            f_min, f_max = filters.min(), filters.max()
+            filters = (filters - f_min) / (f_max - f_min)  
+            print(filters.shape[3])
+            axis_x=1
+            for i in range(filters.shape[3]): 
+                filt=filters[:,:,:, i]
+                plotFilters(filt)   
 # -------------------------------------------------------------------------------------------------
 model = tf.keras.models.Sequential([
-    Convolution2D(filters=32, kernel_size=(3, 3), activation="relu", input_shape=(240, 320, 3)), 
+    Convolution2D(filters=32, kernel_size=(3, 3), activation="relu", input_shape=(150, 150, 3)), 
     MaxPooling2D(pool_size=(2, 2)), 
+    Dropout(0.25),
     Convolution2D(filters=64, kernel_size=(3, 3), activation="relu"), 
     MaxPooling2D(pool_size=(2, 2)), 
+    Dropout(0.25),
     Convolution2D(filters=128, kernel_size=(3, 3), activation="relu"), 
     MaxPooling2D(pool_size=(2, 2)), 
+    Dropout(0.25),
     Flatten(), 
     Dense(128, activation ='relu'),
+    Dropout(0.5),
     Dense(4, activation = "softmax")
 ]) 
 model.summary() 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# ------------------------------------------------------------------------------------------------- 
-model.fit(xTrain, yTrain, batch_size = 5, epochs=100, verbose=1)
-
+model.fit(xTrain, yTrain, batch_size = 5, epochs=3, verbose=1)
 model.save("main.h5")
- 
+# ------------------------------------------------------------------------------------------------- 
+
+model = load_model("main.h5")
