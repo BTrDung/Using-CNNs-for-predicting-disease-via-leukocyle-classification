76fa81117ca18f1479fe7d729ee993bb7249e830
TNT535 <62098728+TNT535@users.noreply.github.com>
2021-06-24 08:07:40 +0700

    vvg16 for 5 classes

--- vgg_train.py
+++ vgg_train.py
@@ -1,15 +1,42 @@
 import tensorflow as tf
 import tensorflow.keras
+from draw_plt_val_accur import draw_plt
+
 from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pathlib
 
-from init_data_train import data_train
+# from init_data_train import data_train
+
 
 # for gpu in tf.config.experimental.list_physical_devices('GPU'):
 #     tf.compat.v2.config.experimental.set_memory_growth(gpu, True)
+# ---------------------split train and val------------------
+train_data_dir = pathlib.Path('data2/5_class')
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   # shear_range=0.2,
+                                   rotation_range=0,
+                                   # zoom_range=0.2,
+                                   vertical_flip=False,
+                                   validation_split=0.2)  # set validation split
 
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(224, 224),
+    batch_size=8,
+    class_mode='categorical',
+    subset='training',
+    seed=20)  # set as training data
+
+validation_generator = train_datagen.flow_from_directory(
+    train_data_dir,  # same directory as training data
+    target_size=(224, 224),
+    batch_size=8,
+    class_mode='categorical',
+    subset='validation')  # set as validation data
 # -------------------------------------------------------------------------------------
 # cache if enough memory
 # AUTOTUNE = tensorflow.data.AUTOTUNE
@@ -20,7 +47,7 @@ from init_data_train import data_train
 model = tf.keras.Sequential()
 
 model.add(Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer='he_uniform', name='block1_conv1',
-                 input_shape=(150, 150, 3)))
+                 input_shape=(224, 224, 3)))
 model.add(Conv2D(64, (3, 3), activation="relu", padding="same", kernel_initializer='he_uniform', name='block1_conv2'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block1_maxpool'))
 
@@ -46,7 +73,7 @@ model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block5_maxpool'))
 model.add(Flatten())
 model.add(Dense(4096, activation='relu'))
 model.add(Dense(4096, activation='relu'))
-model.add(Dense(4, activation='softmax'))
+model.add(Dense(5, activation='softmax'))
 
 
 def optimizer_init_fn():
@@ -66,14 +93,20 @@ model.summary()
 # Fit model (training)
 # history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
 #                               validation_data=val_it, validation_steps=len(val_it), epochs=50, verbose=1)
-
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath='check_point/cp.h5',
+    save_weights_only=False,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True,
+    save_freq="epoch")
+callbacks_list = [model_checkpoint_callback]
 # -------------------------------Train-------------------------
-epoch = 6
-history = model.fit(data_train, batch_size=30, epochs=epoch, verbose=1)
+epoch = 100
+history = model.fit(train_generator, validation_data=validation_generator, batch_size=8, epochs=epoch, verbose=1, callbacks=callbacks_list)
 # model.fit(data_train, validation_data=val_ds, batch_size=30, epochs=30, verbose=1)
 
 model.save("vgg.h5")
 
-
 # ------------------------draw plt if use validation_data----------------------------
-# draw_plt(history, epoch)
+draw_plt(history, epoch)
