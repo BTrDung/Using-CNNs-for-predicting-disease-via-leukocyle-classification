5252c8f5e0d76fae6f37f0a7da4b9bd7772860d0
BTrDung <bambi5465@gmail.com>
2021-07-07 17:20:12 +0700

    delete dataset-master
    delete dataset-master

--- vgg19_train.py
+++ vgg19_train.py
@@ -75,7 +75,7 @@ model.add(Conv2D(512, (3, 3), activation="relu", padding="same", kernel_initiali
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block5_maxpool'))
 
 model.add(Flatten())
-model.add(Dense(4096, activation='relu'))
+model.add(Dense(4096, activation='relu'))   
 model.add(Dropout(0.5))
 model.add(Dense(4096, activation='relu'))
 model.add(Dropout(0.25))
